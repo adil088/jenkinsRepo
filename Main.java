583257f1c13896aa71ae5753b6697ec6b0d8ee5a
aadil <adil88.mohd@gmail.com>
2024-12-18 10:15:17 +0530

    line added

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-public class Main{
-    public static void Main(String[] args){
+public class Main {
+    public static void Main(String[] args) {
         System.out.println("This is java project");
+        System.out.println("This line added after git push");
     }
 }
